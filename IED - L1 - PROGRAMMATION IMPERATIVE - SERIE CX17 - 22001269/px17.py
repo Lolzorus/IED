caa0a8cbfe2e210b81e5e85adffb7c4e737bd433
Lolzorus <131143422+Lolzorus@users.noreply.github.com>
2023-06-13 17:10:00 +0200

    Delete IED - L1 - PROGRAMMATION IMPERATIVE - SERIE CX17 - 22001269 directory

--- IED - L1 - PROGRAMMATION IMPERATIVE - SERIE CX17 - 22001269/px17.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import sys, os
-# argv[1] = minor.bugfix version number
-prog = 'cx17.' + sys.argv[1]
-compile = 'gcc -Wall %s.c -o %s' % (prog, prog)
-if os.system(compile) != 0 : exit
-run = '%s indexe-moi.texte > %s.test' % (prog, prog)
-os.system(run)
